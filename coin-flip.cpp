1de7742c9fa322e16cf1ae9ab595aedf44b695d1
Marek Pelowski <marek.pelowski@protonmail.com>
2023-11-16 18:12:18 +0100

    create an namespace for all variables

---
+++ coin-flip.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <ctime>
+
+namespace vars{
+    int flip;
+    std::string status;
+    int tails;
+    int heads;
+    int CurrentStreakTails;
+    int CurrentStreakHeads;
+    int BestStreakTails;
+    int BestStreakHeads;
+}
+
+int main()
+{
+    using namespace vars;
+
+    return 0;
+}
