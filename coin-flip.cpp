3f7b7f9027820cd5f21d4f20cb814fc5fbbc3f55
Marek Pelowski <marek.pelowski@protonmail.com>
2023-11-20 15:34:20 +0100

    fix the random numbers meter reset on 15 streak

--- coin-flip.cpp
+++ coin-flip.cpp
@@ -3,14 +3,14 @@
 
 namespace vars{
 
-    int flip;
-    std::string status;
-    int tails;
-    int heads;
-    int currentStreakTails;
-    int currentStreakHeads;
-    int bestStreakTails;
-    int bestStreakHeads;
+    double flip;
+    int status;
+    int tails = 0;
+    int heads = 0;
+    int currentStreakTails = 0;
+    int currentStreakHeads = 0;
+    int bestStreakTails = 0;
+    int bestStreakHeads = 0;
 };
 
 void status_check();
@@ -18,12 +18,9 @@ void chance();
 
 int main()
 {
+    srand(time(NULL));
+
     using namespace vars;
-
-    time_t seed;
-
-    time(&seed);
-    srand(seed);
 
     std::cout << "Enter the amount of coin flips: ";
     std::cin >> flip;
@@ -31,6 +28,7 @@ int main()
     for(int i = 0; i < flip; i++){
         chance();
         status_check();
+        //std::cout << "#" << i + 1 << " ";
     }
 
     double tailsScore = (tails * 100.0) / flip;
@@ -47,33 +45,37 @@ int main()
 void status_check(){
     using namespace vars;
 
-    if(status == "tails"){
-        tails++;
+    if(status == 0){
+        tails = tails + 1;
         currentStreakTails++;
-        if(currentStreakTails > bestStreakTails){
+        if(bestStreakTails < currentStreakTails){
             bestStreakTails = currentStreakTails;
         }
         currentStreakHeads = 0;
     }
-    else if(status == "heads"){
-        heads++;
+    else if(status == 1){
+        heads = heads + 1;
         currentStreakHeads++;
-        if(currentStreakHeads > bestStreakHeads){
+        if(bestStreakHeads < currentStreakHeads){
             bestStreakHeads = currentStreakHeads;
         }
         currentStreakTails = 0;
+    } else {
+        // Add these lines to reset streaks in case status is neither "tails" nor "heads"
+        currentStreakTails = 0;
+        currentStreakHeads = 0;
     }
 }
 
-void chance(){
+void chance() {
     using namespace vars;
 
-    int x = rand() % 2;
+    int x = rand() % 10 + 1;
 
-    if(!x){
-        status = "tails";
+    if (x <= 5) {
+        status = 0;
     }
-    else if(x){
-        status = "heads";
+    else {
+        status = 1;
     }
 }
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,2 @@
+
+        currentStreakHeads = 0;
